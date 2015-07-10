8a1460a24d58475655d536925b279ce69ac38f26
Amitai Burstein <amitai@gizra.com>
2015-07-11 00:32:43 +0300

    generic resource

--- src/services/companies.js
+++ src/services/companies.js
@@ -1,51 +1,8 @@
-import {inject} from 'aurelia-framework';
-import {EventAggregator} from 'aurelia-event-aggregator';
-import {WebAPI} from './backend-http';
+import {ResourceAbstract} from './resource';
 
-@inject(EventAggregator, WebAPI)
-export class CompaniesAPI {
-
-  // Internal cache.
-  cache = null;
+export class CompaniesAPI extends ResourceAbstract {
 
   endpoint = 'api/v1.0/companies';
 
-  constructor(eventAggregator, http) {
-    this.eventAggregator = eventAggregator;
-    this.http = http.http;
-
-    this.subscribeEvents();
-  }
-
-  get() {
-    var cache = this.getCache();
-    if (!!cache) {
-      console.log('companies from cache');
-      return Promise.resolve(cache);
-    }
-
-    console.log('companies from server');
-    return this.http
-      .get(this.endpoint)
-      .then(response => {
-        var data = JSON.parse(response.response).data;
-        this.setCache(data);
-        return data;
-      });
-  }
-
-  setCache(data) {
-    this.cache = data;
-  }
-
-  getCache() {
-    return this.cache;
-  }
-
-  subscribeEvents() {
-    this.eventAggregator.subscribe('clear_cache', payload => {
-      console.log('clear cache companies');
-      this.cache = null;
-    });
-  }
+  resourceName = 'companies';
 }
